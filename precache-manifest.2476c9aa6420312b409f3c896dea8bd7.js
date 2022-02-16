0019a90b0cf770ce887d0a87ee7e91f491b7c82a
Ifeanyi Ofikulu <ifeanyiofi@gmail.com>
2022-02-16 23:49:12 +0100

    Updates

---
+++ precache-manifest.2476c9aa6420312b409f3c896dea8bd7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8175872383538d98844f407ec92ca47",
+    "url": "/Basic-ecommerce/index.html"
+  },
+  {
+    "revision": "c0c03667da76c5409aa2",
+    "url": "/Basic-ecommerce/static/css/2.98705a83.chunk.css"
+  },
+  {
+    "revision": "58632c59a35679696480",
+    "url": "/Basic-ecommerce/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "c0c03667da76c5409aa2",
+    "url": "/Basic-ecommerce/static/js/2.ecccfa30.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Basic-ecommerce/static/js/2.ecccfa30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58632c59a35679696480",
+    "url": "/Basic-ecommerce/static/js/main.0171421f.chunk.js"
+  },
+  {
+    "revision": "0ede8432b364ce24b05b",
+    "url": "/Basic-ecommerce/static/js/runtime-main.c7f550e8.js"
+  }
+]);
